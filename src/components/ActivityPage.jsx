cfaf5d63bdcee4345aee8fb031dafad29dfdd7f2
EktaKumari3004 <ektak9341@gmail.com>
2026-01-11 01:24:55 +0530

    Update ActivityPage.jsx

--- src/components/ActivityPage.jsx
+++ src/components/ActivityPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
-import { activities } from '../data/activities';
+import { activities } from '../components/data/activities';
 
 export default function ActivityPage() {
     const { domainId } = useParams();
